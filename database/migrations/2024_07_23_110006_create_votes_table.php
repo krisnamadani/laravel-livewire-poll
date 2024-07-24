b8d415d7436f975fef2af09ada00e00ac5a40b83
krisna <krisnamadani@gmail.com>
2024-07-24 21:03:04 +0700

    voting in polls

--- database/migrations/2024_07_23_110006_create_votes_table.php
+++ database/migrations/2024_07_23_110006_create_votes_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('votes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->timestamps();
 
             $table->foreignIdFor(\App\Models\Option::class)->constrained();
